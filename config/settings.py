7a6ccf21dd228dfc151d9f30e1d53cca1eb77fb9
Al Pogorelov <eczemplrez@gmail.com>
2025-02-07 18:34:28 +0600

    1

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mailing_manager',
-    'user_manager',
-    'message_manager',
+
 ]
 
 MIDDLEWARE = [
